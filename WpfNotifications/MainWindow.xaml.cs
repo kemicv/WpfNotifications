ad9d0b4b6e57069a80df1b8d0646e55e1a8939f3
kemicv <kemicv@gmail.com>
2024-06-11 11:10:35 -0600

    Add project files.

---
+++ WpfNotifications/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using WpfNotifications.Models;
+
+namespace WpfNotifications
+{
+    public partial class MainWindow : Window
+    {
+        readonly IList<Car> _cars = new List<Car>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _cars.Add(new Car { Id = 1, Color = "Blue", Make = "Chevy", PetName = "Kit" });
+            _cars.Add(new Car { Id = 2, Color = "Red", Make = "Ford", PetName = "Red Rider" });
+            cboCars.ItemsSource = _cars;
+        }
+
+        private void BtnChangeColor_OnClick(object sender, RoutedEventArgs e)
+        {
+            //Use the SelectedItem property of the ComboBox to locate the
+            //selected record from the car list and change the color
+            _cars.First(x => x.Id == ((Car)cboCars.SelectedItem)?.Id).Color = "Pink";
+        }
+
+        private void btnAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            //Add an event handler to the btnAddCar button
+            var maxCount = _cars?.Max(x => x.Id) ?? 0;
+            _cars?.Add(new Car { Id = ++maxCount, Color = "Yellow", Make = "VW", PetName = "Birdie" });
+        }
+    }
+}
---
+++ WpfNotifications/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfNotifications.Models
+{
+    public class Car : INotifyPropertyChanged
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { if (value == _id) return; _id = value; OnPropertyChanged(); }
+        }
+
+        public string Make { get; set; }
+        
+        private string _color;
+        public string Color
+        {
+            get { return _color; }
+            set { if (value == _color) return; _color = value; OnPropertyChanged(); }
+        }
+
+        public string PetName { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this,
+            new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
